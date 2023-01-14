30ee480cbe2b267905ead9df825c87909d7d6b31
Martin Larralde <martin.larralde@embl.de>
2023-01-14 17:12:12 +0100

    Fix generation of directed walk for polytrees in `TreeMatrix`

--- torch_treecrf/tree.py
+++ torch_treecrf/tree.py
@@ -1,8 +1,31 @@
 import collections
 import typing
-from typing import Iterator, Union
+from typing import Iterator, Union, Sequence
 
 import torch
+
+
+class _VariableColumnStorage:
+    """A compressed storage for tensors with variable number of columns.
+    """
+
+    def __init__(self, data: Sequence[torch.Tensor], device: Union[torch.device, str, None]=None, dtype: Union[torch.dtype, str, None]=None):
+        self.indices = torch.zeros(len(data)+1, device=device, dtype=torch.long)
+        self.data = torch.zeros(sum(len(row) for row in data), device=device, dtype=dtype)
+
+        for i, row in enumerate(data):
+            self.indices[i+1] = self.indices[i] + len(row)
+            self.data[self.indices[i]:self.indices[i+1]] = torch.as_tensor(row)
+
+    def __len__(self):
+        return self.indices.shape[0] - 1
+
+    def __getitem__(self, index: Union[int, slice]):
+        if isinstance(index, slice):
+            return type(self)([self[i] for i in range(*index.indices(len(self)))])
+        start = self.indices[index]
+        end = self.indices[index+1]
+        return self.data[start:end]
 
 
 class TreeMatrix:
@@ -10,31 +33,25 @@ class TreeMatrix:
     """
 
     def __init__(self, data: torch.Tensor, device: Union[torch.device, str, None]=None):
-        # store data
+        # store data as sparse matrix
         _data = torch.as_tensor(data, dtype=torch.int32, device=device)
-
-        # get the index of the root
-        self.roots = torch.where(_data.sum(axis=1) == 0)[0]
-        if len(self.roots) == 0:
-            raise ValueError(f"Failed to find roots in tree: {self.roots}")
+        self.data = _data.to_sparse()
 
         # cache parents and children
-        self._parents = []
-        self._children = []
-        for i in range(len(self)):
-            self._parents.append( torch.where(_data[i, :] != 0)[0] )
-            self._children.append( torch.where(_data[:, i] != 0)[0] )
+        self._parents = _VariableColumnStorage(
+            [torch.where(_data[i, :] != 0)[0] for i in range(_data.shape[0])],
+            device=device,
+            dtype=torch.long
+        )
+        self._children = _VariableColumnStorage(
+            [torch.where(_data[:, i] != 0)[0] for i in range(_data.shape[0])],
+            device=device,
+            dtype=torch.long
+        )
 
         # run a BFS walk and store indices
-        done = set()
-        self.bfs_path = -torch.ones(_data.shape[0], dtype=torch.int32, device=device)
-        n = 0
-        q = collections.deque(self.roots)
-        while q:
-            node = q.popleft()
-            q.extend(self.children(node))
-            self.bfs_path[n] = node
-            n += 1
+        self._down = self._roots_to_leaves(_data)
+        self._up = self._leaves_to_root(_data)
 
         # store data as a sparse matrix
         self.data = _data.to_sparse()
@@ -43,10 +60,72 @@ class TreeMatrix:
         return self.data.shape[0]
 
     def __iter__(self) -> Iterator[int]:
-        return iter(self.bfs_path)
+        return iter(self._down)
 
     def __reversed__(self) -> Iterator[int]:
-        return reversed(self.bfs_path)
+        return iter(self._up)
+
+    def _roots_to_leaves(self, data: torch.Tensor) -> torch.Tensor:
+        """Generate a walk order to explore the tree from roots to leaves.
+        """
+        path = -torch.ones(data.shape[0], dtype=torch.long, device=data.device)
+        roots = torch.where(data.sum(axis=1) == 0)[0]
+
+        n = 0
+        todo = collections.deque(roots)
+        done = torch.zeros(data.shape[0], dtype=torch.bool, device=data.device)
+
+        while todo:
+            # get next node to visit
+            i = todo.popleft()
+            # skip if done already
+            if done[i]:
+                continue
+            # delay node visit if we didn't visit some of its children yet
+            if not torch.all(done[self.parents(i)]):
+                todo.append(i)
+                continue
+            # add node parents
+            todo.extend(self.children(i))
+            # mark node as done
+            done[i] = True
+            path[n] = i
+            n += 1
+
+        assert n == len(path)
+        assert done.count_nonzero() == data.shape[0]
+        return path
+
+    def _leaves_to_root(self, data: torch.Tensor) -> torch.Tensor:
+        """Generate a walk order to explore the tree from leaves to roots.
+        """
+        path = -torch.ones(data.shape[0], dtype=torch.long, device=data.device)
+        roots = torch.where(data.sum(axis=0) == 0)[0]
+
+        n = 0
+        todo = collections.deque(roots)
+        done = torch.zeros(data.shape[0], dtype=torch.bool, device=data.device)
+
+        while todo:
+            # get next node to visit
+            i = todo.popleft()
+            # skip if done already
+            if done[i]:
+                continue
+            # delay node visit if we didn't visit some of its children yet
+            if not torch.all(done[self.children(i)]):
+                todo.append(i)
+                continue
+            # add node parents
+            todo.extend(self.parents(i))
+            # mark node as done
+            done[i] = True
+            path[n] = i
+            n += 1
+
+        assert n == len(path)
+        assert done.count_nonzero() == data.shape[0]
+        return path
 
     def parents(self, i: int) -> torch.Tensor:
         return self._parents[i]
